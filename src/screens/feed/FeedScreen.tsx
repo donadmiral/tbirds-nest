a5f13807479d368951599b68f16858b97c5196d7
Don <donpanshemusa13@gmail.com>
2026-08-22 19:37:13 -0700

    Composer picker exports H264 and JPEG via compatible asset mode so posted videos play in browsers

--- src/screens/feed/FeedScreen.tsx
+++ src/screens/feed/FeedScreen.tsx
@@ -990,6 +990,7 @@ export default function FeedScreen({ navigation }: any) {
         mediaTypes: ['images', 'videos'] as ImagePicker.MediaType[],
         allowsMultipleSelection: true,
         selectionLimit: 10,
+        preferredAssetRepresentationMode: ImagePicker.UIImagePickerPreferredAssetRepresentationMode.Compatible,
         quality: 1,
         allowsEditing: false,
         exif: false,
